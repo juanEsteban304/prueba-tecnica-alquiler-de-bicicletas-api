3381597da6ecfd36f7e443a4eafa5aedb386f1df
juanEsteban304 <juanesgarciacastro@gmail.com>
2026-07-31 15:51:48 -0500

    Se Implemento finalizacion y calculo de alquileres con logica de negocio

--- alquilerbicicletas/src/main/java/co/com/ceiba/alquilerbicicletas/infrastructure/adapter/out/persistence/AlquilerJpaRepositorio.java
+++ alquilerbicicletas/src/main/java/co/com/ceiba/alquilerbicicletas/infrastructure/adapter/out/persistence/AlquilerJpaRepositorio.java
@@ -1,9 +1,12 @@
 package co.com.ceiba.alquilerbicicletas.infrastructure.adapter.out.persistence;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AlquilerJpaRepositorio extends JpaRepository<AlquilerEntidad, Long>{
 
+    Optional<AlquilerEntidad> findByBicicleta_CodigoAndHoraFinIsNull(String codigoBicicleta);
 }
--- alquilerbicicletas/src/main/java/co/com/ceiba/alquilerbicicletas/infrastructure/adapter/out/persistence/AlquilerRepositorioAdaptador.java
+++ alquilerbicicletas/src/main/java/co/com/ceiba/alquilerbicicletas/infrastructure/adapter/out/persistence/AlquilerRepositorioAdaptador.java
@@ -1,25 +1,34 @@
 package co.com.ceiba.alquilerbicicletas.infrastructure.adapter.out.persistence;
 
-import org.springframework.stereotype.Component;
+import java.util.Optional;
 
 import co.com.ceiba.alquilerbicicletas.domain.model.Alquiler;
 import co.com.ceiba.alquilerbicicletas.domain.ports.out.AlquilerRepositorioComandoPuerto;
 
-@Component
 public class AlquilerRepositorioAdaptador implements AlquilerRepositorioComandoPuerto{
     
     private final AlquilerJpaRepositorio jpaRepositorio;
     private final AlquilerEntidadMapeador mapeador;
     private final BicicletaJpaRepositorio bicicletaJpaRepositorio;
+    private final AlquilerMapeador mapeadorActualizar;
 
-    public AlquilerRepositorioAdaptador(AlquilerJpaRepositorio jpaRepositorio, AlquilerEntidadMapeador mapeador, BicicletaJpaRepositorio bicicletaJpaRepositorio) {
+    public AlquilerRepositorioAdaptador(AlquilerJpaRepositorio jpaRepositorio, AlquilerEntidadMapeador mapeador, 
+                                        BicicletaJpaRepositorio bicicletaJpaRepositorio, AlquilerMapeador mapeadorActualizar) {
         this.jpaRepositorio = jpaRepositorio;
         this.mapeador = mapeador;
         this.bicicletaJpaRepositorio = bicicletaJpaRepositorio;
+        this.mapeadorActualizar = mapeadorActualizar;
     }
 
     @Override
     public Alquiler guardar(Alquiler alquiler){
+        Optional<AlquilerEntidad>alquilerExistente = jpaRepositorio.findByBicicleta_CodigoAndHoraFinIsNull(alquiler.getBicicleta().getCodigo());
+        if (alquilerExistente.isPresent()){
+            AlquilerEntidad entidad = alquilerExistente.get();
+            mapeadorActualizar.actualizarEntidad(entidad, alquiler);
+            AlquilerEntidad actualizado = jpaRepositorio.save(entidad);
+            return mapeador.convertirADTO(actualizado);
+        }
         BicicletaEntidad bicicletaEntidad = bicicletaJpaRepositorio.getReferenceById(alquiler.getBicicleta().getCodigo());
         AlquilerEntidad  entidad = mapeador.convertirAEntidad(alquiler, bicicletaEntidad);
         AlquilerEntidad guardar = jpaRepositorio.save(entidad);
